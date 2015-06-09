d15109464d95c84504ce8c8dbf8a6c3e1905b62e
Andrew Magill <andrew.b.magill@gmail.com>
2015-06-08 20:31:04 -0500

    adding custom permissions to views

--- snippets/views.py
+++ snippets/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework import permissions
 from snippets.models import Snippet
+from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer
 from snippets.serializers import UserSerializer
 
@@ -11,7 +12,8 @@ class SnippetList(generics.ListCreateAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -22,7 +24,8 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
 
 class UserList(generics.ListAPIView):
     """
